4913d375f185b08eec504e28913b088c91d398cc
ardianilyas <ardianilyas@gmail.com>
2025-01-25 12:09:32 +0700

    ui: create button-create UI component

---
+++ resources/js/components/ButtonCreate.vue
@@ -0,0 +1,11 @@
+<template>
+  <Button class="mb-3">
+    <PlusIcon />
+    <slot />
+  </Button>
+</template>
+
+<script setup>
+import { PlusIcon } from "@radix-icons/vue";
+import { Button } from "@/components/ui/button/index.js";
+</script>
