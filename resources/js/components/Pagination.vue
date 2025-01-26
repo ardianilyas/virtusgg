ca066604203b73e7b6d9715c1f6147f3cf9f9dda
ardianilyas <ardianilyas@gmail.com>
2025-01-26 21:39:26 +0700

    update: update pagination component

--- resources/js/components/Pagination.vue
+++ resources/js/components/Pagination.vue
@@ -1,15 +1,20 @@
 <template>
   <nav class="my-12 relative flex justify-center">
-    <div class="flex justify-center space-x-4">
-      <Link preserve-scroll v-if="links.prev_page_url != null" :href="links.prev_page_url" class="text-gray-800 hover:text-blue-600" :class="{ '!text-gray-500' : links.prev_page_url == null }"> Previous </Link>
+    <div class="flex justify-center items-center space-x-4">
+      <Button v-if="links.prev_page_url">
+        <Link preserve-scroll :href="links.prev_page_url"> Prev </Link>
+      </Button>
       <p> {{ links.current_page }} / {{ links.last_page }} </p>
-      <Link preserve-scroll v-if="links.next_page_url" :href="links.next_page_url" class="text-gray-800 hover:text-blue-600" :class="{ '!text-gray-500' : links.next_page_url == null }"> Next </Link>
+      <Button v-if="links.next_page_url">
+        <Link preserve-scroll :href="links.next_page_url"> Next </Link>
+      </Button>
     </div>
   </nav>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3'
+import { Button } from "@/components/ui/button/index.js";
 defineProps({
   links: Object
 })
